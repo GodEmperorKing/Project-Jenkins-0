18a05f513067cd16551460294c6c2675f6250b97
GodEmperorKing <youralltoeasy@gmail.com>
2026-03-29 21:35:59 -0700

    Remove S3 backend configuration
    Removed S3 backend configuration from Terraform file.

--- test-bucket.tf
+++ test-bucket.tf
@@ -4,14 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0" # Use latest version if possible
     }
-  }
-  backend "s3" {
-    bucket  = "terraform-state-aaronmcd"                 # Name of the S3 bucket
-    key     = "jenkins-test-031726.tfstate"        # The name of the state file in the bucket
-    region  = "us-east-2"                          # Use a variable for the region
-    encrypt = true                                 # Enable server-side encryption (optional but recommended)
-  }
-}
 
 provider "aws" {
   region  = "us-east-1"
