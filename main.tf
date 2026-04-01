b268c9351ea2808701e3454ed21aaf85789c2afa
GodEmperorKing <youralltoeasy@gmail.com>
2026-03-31 18:09:00 -0700

    Remove public-read ACL from S3 configuration
    Remove public-read ACL from S3 object configuration.

--- main.tf
+++ main.tf
@@ -14,5 +14,4 @@ resource "aws_s3_object" "screenshots" {
   bucket = aws_s3_bucket.my_project_bucket.id
   key    = each.value
   source = "${path.module}/${each.value}"
-  acl    = "public-read" # This makes them public automatically!
-}
+ }
